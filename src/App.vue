30f41c7406f17803337a233483f93c83f55ebab0
Svetlana Trukhina <sweta4ca@mail.ru>
2024-02-18 05:07:47 +0700

    feat: solve directives 02 v-if

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <Header />
   <Catalog />
 </template>
 
 <script setup lang="ts">
+import Header from "./components/HeaderComponent.vue";
 import Catalog from "./components/CatalogComponent.vue";
 </script>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <button v-if="isLogin" @click="isLogin = false" class="btn">Войти</button>
+    <button v-else @click="isLogin = true" class="btn">Выйти</button>
+    <img v-if="isLogin" src="../assets/user.png" class="icon icon-login" />
+    <img v-else src="../assets/arrow.png" class="icon icon-logout" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isLogin = ref(true);
+</script>
+
+<style scoped>
+.header {
+  background-color: rgb(251, 255, 3);
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 20px;
+}
+.btn {
+  width: 85px;
+  height: 30px;
+}
+.icon {
+  max-width: 40px;
+}
+</style>
