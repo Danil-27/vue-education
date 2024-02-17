e9f42114ef374fb006b9833d05e5a4a2834e8bd3
Danil-27 <pde200126@gmail.com>
2024-02-17 17:46:49 +0300

    tasks: two done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="app">
+    <!-- Реализованно спомощью v-if - v-else  -->
+    <header class="header">
+      <img class="header-img" v-if="showElement" :src="images" alt="picture" />
+      <img class="header-img" v-else :src="images" alt="picture" />
+      <div class="header__block">
+        <button @click="showClickIf" class="showClickIf">
+          Клик <strong>IF</strong>
+        </button>
+        <button @click="showClickElse" class="showClickIf">
+          Клик <strong>else</strong>
+        </button>
+      </div>
+    </header>
+    <!-- Реализованно спомощью @clik -->
+    <!-- <header class="header">
+      <img class="header-img" :src="images" alt="Картинка" />
+      <div class="header__block">
+        <button @click="enter" class="enter">войти</button>
+        <button @click="exit" class="exit">выйти</button>
+      </div>
+    </header> -->
     <p class="tasks" v-if="items.length > 5">
       УРа Задание выполнено здесь <strong class="strong">больше</strong> 5
       элиментов !
@@ -10,6 +31,8 @@
     </p>
     <div class="for" v-for="item in items" v-bind:key="item">
       <div class="for_item">
+        <span>{{ item.title }}</span>
+        <button @click="removeObject(item)" class="showClickIf">удалить</button>
         <h2 class="heading">{{ item.title }}</h2>
         <img class="img" :src="item.img" alt="car" />
         <p class="description">{{ item.description }}</p>
@@ -22,6 +45,9 @@
 export default {
   data() {
     return {
+      showElement: false,
+      images:
+        "https://thumbs.dreamstime.com/b/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-%D0%BF%D1%83%D1%81%D1%82%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F-%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0-%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%BE%D0%B5-%D0%B4%D0%BB%D1%8F-%D1%81%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2-208480728.jpg",
       items: [
         {
           id: 1,
@@ -68,6 +94,32 @@ export default {
       ],
     };
   },
+  methods: {
+    exit() {
+      this.images =
+        "https://thumbs.dreamstime.com/b/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-%D0%BF%D1%83%D1%81%D1%82%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F-%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0-%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%BE%D0%B5-%D0%B4%D0%BB%D1%8F-%D1%81%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2-208480728.jpg";
+    },
+    enter() {
+      this.images =
+        "https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg";
+    },
+    showClickIf() {
+      this.showElement = true;
+      this.images =
+        "https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg";
+    },
+    showClickElse() {
+      this.showElement = false;
+      this.images =
+        "https://thumbs.dreamstime.com/b/%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-%D0%BF%D1%83%D1%81%D1%82%D0%BE%D0%B3%D0%BE-%D0%BF%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8F-%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0-%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%BE%D0%B5-%D0%B4%D0%BB%D1%8F-%D1%81%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85-%D1%81%D0%B0%D0%B9%D1%82%D0%BE%D0%B2-208480728.jpg";
+    },
+    removeObject(items) {
+      const index = this.items.indexOf(items);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+    },
+  },
 };
 </script>
 
@@ -77,9 +129,33 @@ export default {
   margin: 0;
   padding: 0;
 }
+strong {
+  color: blueviolet;
+}
+.showClickIf {
+  background-color: blanchedalmond;
+  padding: 10px 20px;
+}
 .app {
   background-image: url(https://bgfons.com/uploads/ornaments/ornaments_texture1133.jpg);
 }
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100px;
+  background-color: rgb(114, 64, 29);
+}
+.header-img {
+  border-radius: 50%;
+  height: 80px;
+  width: 80px;
+}
+.header__block {
+  display: flex;
+  gap: 0px 20px;
+}
+
 .tasks,
 .elseTasks {
   font-size: 40px;
