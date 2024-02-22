03900f12e2ec6e42a1dded3cef625f04e63949bc
Svetlana Trukhina <sweta4ca@mail.ru>
2024-02-22 10:28:32 +0700

    feat: solve v-bind 01

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <Header />
-  <Catalog />
+  <!-- <Header /> -->
+  <!-- <Catalog /> -->
+  <VBind />
 </template>
 
 <script setup lang="ts">
-import Header from "./components/HeaderComponent.vue";
-import Catalog from "./components/CatalogComponent.vue";
+// import Header from "./components/HeaderComponent.vue";
+// import Catalog from "./components/CatalogComponent.vue";
+import VBind from "./components/VBind.vue";
 </script>
 
 <style scoped></style>
---
+++ src/components/VBind.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="tasks">
+    <div class="tasks__one task">
+      <div>Task 1</div>
+      <div class="img-wrapper">
+        <img
+          v-bind:src="link || defaultImage"
+          alt="uploaded picture"
+          class="task__img"
+        />
+      </div>
+
+      <input
+        v-model="link"
+        placeholder="htttps://pic.ru/cat.jpg"
+        class="task__input"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const link = ref("");
+const defaultImage =
+  "https://f8.pmo.ee/wzKYitGs5fJ_6bi3QRd7YJeHwkk=/685x0/nginx/o/2022/08/03/14736428t1h4a35.jpg";
+</script>
+
+<style scoped>
+.tasks {
+  background: -webkit-linear-gradient(#ffe894, #ffccf8);
+  padding: 25px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.task {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+  padding: 10px 7px;
+  width: fit-content;
+}
+.img-wrapper {
+  width: 280px;
+  height: 180px;
+
+  border: 3px solid whitesmoke;
+  border-radius: 6px;
+  overflow: hidden;
+}
+.task__img {
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
+  object-position: center;
+  transition: 0.3s;
+}
+.task__img:hover {
+  transform: scale(1.1);
+}
+.task__input {
+  border: none;
+  border-radius: 4px;
+  padding: 5px 15px;
+}
+</style>
