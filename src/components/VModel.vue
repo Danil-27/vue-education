9081c651cdbc96e76857869aeb2c59f19938be11
Svetlana Trukhina <sweta4ca@mail.ru>
2024-02-28 22:58:22 +0700

    feat: solve v-model 02

--- src/components/VModel.vue
+++ src/components/VModel.vue
@@ -3,6 +3,13 @@
     <input v-model="userNum" type="number" />
     <div>Число: {{ userNum }}</div>
     <div>Какое: {{ res }}</div>
+
+    <div>
+      <div>Сложение чисел:</div>
+      <input v-model="firstNum" type="number" /> +
+      <input v-model="secondNum" type="number" /> =
+      {{ sum }}
+    </div>
   </div>
 </template>
 
@@ -12,11 +19,18 @@ import { ref, watch } from "vue";
 const userNum = ref(0);
 const res = ref("");
 
+const firstNum = ref(0);
+const secondNum = ref(0);
+const sum = ref(0);
+
 watch(userNum, () => {
   if (Math.sign(userNum.value) === 1) {
     res.value = "positive";
   } else if (Math.sign(userNum.value) === -1) {
     res.value = "negative";
   } else res.value = "zero!";
+});
+watch([firstNum, secondNum], ([newFirst, newSecond]) => {
+  sum.value = newFirst + newSecond;
 });
 </script>
