7da9c54936999ed1909e4e46f7942e836aab8320
Danil-27 <pde200126@gmail.com>
2024-03-15 20:20:39 +0300

    feat: task done one

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="app">
-    <VmodelMainTaskThree />
+  <div class="app__wrapper app__wrapper_page-type_inner">
+    <!-- LEFTBAR -->
+    <div class="left-bar">
+      <RouterLink to="/">Home ссылка `/`</RouterLink>
+      <br />
+      <RouterLink to="/Vmodel">Routing ссылка `/Vmodel`</RouterLink>
+      <br />
+    </div>
+    <div class="app__main">
+      <!-- CONTENT -->
+      DD GOOD
+      <RouterView />
+      <VifBody></VifBody>
+    </div>
   </div>
 </template>
+I
 
 <script setup>
-import VmodelMainTaskThree from './components/V-model/VmodelMainTaskThree.vue';
+// import VmodelMainTaskThree from './components/V-model/VmodelMainTaskThree.vue';
+// import Vb from './components/V-bind/VbindMain.vue';
+import { ref } from 'vue';
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'normalize.css';
 
-createApp(App).mount('#app');
+import commentsOne from './components/V-bind/VbindMain.vue';
+import commentsTwo from './components/V-model/VmodelMainTaskThree.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      name: 'VbindMain',
+      path: '/',
+      component: commentsOne
+    },
+    {
+      name: 'Vmodel',
+      path: '/Vmodel',
+      component: commentsTwo
+    }
+  ]
+});
+createApp(App).use(router).mount('#app');
