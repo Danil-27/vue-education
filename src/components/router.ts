a33f04aa3d204f75bac21af0b7d4eb188b25f116
Svetlana Trukhina <sweta4ca@mail.ru>
2024-03-16 22:19:59 +0700

    fix: replace memory history to web history

--- src/components/router.ts
+++ src/components/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import Home from './homePage.vue'
 import Catalog from './catalogPage.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes
 })
 
