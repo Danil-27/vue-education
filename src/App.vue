b84a44f4ea77745f331d6efd2362194c160cfef3
Danil-27 <pde200126@gmail.com>
2024-03-17 22:02:02 +0300

    feat: slot task one

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <div class="container">
+    <SlotsMainTwo></SlotsMainTwo>
+  </div>
 </template>
 
-<script></script>
+<script setup>
+// import SlotsMain from '@/components/SlotsMainOne.vue';
+import SlotsMainTwo from '@/components/SlotsMainTwo.vue';
+</script>
 
-<style scoped></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  max-width: 700px;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
---
+++ src/components/slotTasks/SlotChildTwo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>ss</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.slot {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: burlywood;
+  height: 80vh;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
