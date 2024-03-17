cc1f1bbb9531b7a63a2d0bdd80b93b0ffdeebeaf
Danil-27 <pde200126@gmail.com>
2024-03-17 18:55:13 +0300

    fix: merge

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'normalize.css'
-
-createApp(App).mount('#app')
-=======
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'normalize.css';
 
 createApp(App).mount('#app');
->>>>>>> main
