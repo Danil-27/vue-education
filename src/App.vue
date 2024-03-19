710393b9d3466a887a241b8d10c0a166f1ad5871
Svetlana Trukhina <sweta4ca@mail.ru>
2024-03-19 23:21:53 +0700

    feat: solve 03 slot

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="btns">
+      <button @click="isModalOpen = !isModalOpen">show \ hide modal!</button>
+      <button @click="modalContent = soup">set Soup to Modal</button>
+      <button @click="modalContent = kasha">set Kasha to Modal</button>
+    </div>
+    <Modal v-show="isModalOpen">
+      {{ modalContent }}
+    </Modal>
+  </div>
 </template>
 
-<script></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Modal from './components/ModalComponent.vue';
 
-<style scoped lang="scss"></style>
+const isModalOpen = ref(false);
+const modalContent = ref('base modal content from App!');
+
+const soup = 'this is soup!';
+const kasha = 'awesome appetit kasha!!';
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="modal">
+    <slot>default modal content</slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.modal {
+  border: 2px solid red;
+  width: 250px;
+  height: 250px;
+  margin: auto;
+}
+</style>
